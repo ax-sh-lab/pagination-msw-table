4d202e964842f154dc075e625dc14286f4e675cb
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-05-23 03:23:08 +0545

    add react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import { AppProps } from 'next/app'
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import React from 'react'
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   require('../mocks')
 }
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient())
+  return  <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}> 
+              <Component {...pageProps} />    
+            </Hydrate>
+          </QueryClientProvider>
 }
