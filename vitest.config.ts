5a8ca0332e498cb43664404882641e89c3acc5eb
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-11-04 22:36:27 +0545

    feat(mock): add unit test for computeTotalPages function (feature/data-table)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,16 @@
 
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
-    environment: "jsdom",
+    environment: "happy-dom",
     globals: true,
+    include: ["**/*.test.{ts,tsx}"],
+    exclude: ["node_modules", "**/*.stories.{ts,tsx}"],
+    setupFiles: ["./setup-tests.ts"],
   },
 });
