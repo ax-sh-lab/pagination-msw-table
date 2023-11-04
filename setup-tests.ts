76d200ad2ab3c36882984a56e65652d0d7de0ad6
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-11-04 22:59:13 +0545

    feat: setup tests

--- setup-tests.ts
+++ setup-tests.ts
@@ -4,5 +4,6 @@ import "@testing-library/jest-dom/vitest";
 
 // NOTE: runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
+  console.log("[setup-tests: Trigger afterEach]");
   cleanup();
 });
