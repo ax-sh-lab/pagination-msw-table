73260b964b7a08380177b3e305220cd28da26163
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-11-04 23:49:43 +0545

    feat(test): add TestQueryClientWrapper component (feature/data-table)
    Add a new file `test-query-client.wrapper.tsx` in the `src/ui` directory.
    
    The `TestQueryClientWrapper` component is responsible for wrapping the children components with the `QueryClientProvider` from the `@tanstack/react-query` library. It creates a new instance of `QueryClient` with the option to turn retries off.
    
    This component is useful for testing React Query and preventing timeouts during tests.
    
    Issue: #feature/data-table

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+// Validate schema on build (recommended)
+// We recommend you importing your newly created file in your next.config.mjs. This will make sure your environment variables are validated at build time which will save you a lot of time and headaches down the road.
+import "./env";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
