e1afec95b66709acea12eceb91c8f055d7da1db3
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-11-04 22:47:24 +0545

    feat(utils): add computeTotalPages function (feature/data-table)
    - Added computeTotalPages function to calculate the total number of pages
    based on the total number of records and records per page.
    - Added test cases to verify the functionality of computeTotalPages function.
    - Added test case for handling NaN input and returning 0 as the result.
    
    Issue: #feature/data-table

--- src/mocks/utils/compute-total-pages.test.ts
+++ src/mocks/utils/compute-total-pages.test.ts
@@ -1,9 +1,16 @@
 import { computeTotalPages } from "@/mocks/utils/compute-total-pages";
 
 describe(computeTotalPages.name, () => {
-  const v = computeTotalPages(10, 5);
-  console.log(v);
   it("should return 2", () => {
-    expect(v).toBe(2);
+    const pages = computeTotalPages(10, 5);
+    expect(pages).toBe(2);
+  });
+  it("should return 0 for not a number", () => {
+    const pages = computeTotalPages(Number(""), 5);
+    expect(pages).toBe(0);
+  });
+  it("should return 0 for not a nan", () => {
+    const pages = computeTotalPages(NaN, 5);
+    expect(pages).toBe(0);
   });
 });
