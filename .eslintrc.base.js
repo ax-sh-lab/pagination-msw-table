fe555631d53324ae0199cb4a4699fa1486d2bd6e
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-11-06 03:38:18 +0545

    chore(eslint): update .eslintrc.base.js and package.json
    - Update extends in .eslintrc.base.js
    - Add 'plugin:import/recommended' to extends in .eslintrc.base.js
    - Add 'plugin:import/errors' and 'plugin:import/warnings' to extends in .eslintrc.base.js
    - Update lint script in package.json to use 'stylish' formatter
    - Add 'lint:table' script in package.json to use 'table' formatter
    - Update lint:pretty script in package.json
    
    (feature/eslint-plugin-management)

--- .eslintrc.base.js
+++ .eslintrc.base.js
@@ -2,7 +2,14 @@ const { defineConfig } = require("eslint-define-config");
 const prettierConfig = require("eslint-config-prettier");
 
 module.exports = defineConfig({
-  extends: ["./.eslintrc.prettier.js"],
+  extends: [
+    "./.eslintrc.prettier.js",
+
+    "plugin:import/recommended",
+    // alternatively, 'recommended' is the combination of these two rule sets:
+    // "plugin:import/errors",
+    // "plugin:import/warnings",
+  ],
 
   plugins: [
     "@tanstack/query",
