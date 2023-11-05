68ab4e02a3bae0852689df29f0ef97a92c8580b1
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-11-05 16:58:30 +0545

    feat(users-list-mock-handler): Add pagination to users list mock handler (pagination-msw-table)

--- src/mocks/handlers/users-list-mock-handler.ts
+++ src/mocks/handlers/users-list-mock-handler.ts
@@ -8,35 +8,40 @@ import { range } from "lodash";
 import { PaginationResponse } from "@/types";
 import { mockAPIBaseJoinPath } from "@/mocks";
 
-export const usersListMockHandler = http.get(
-  mockAPIBaseJoinPath(API_ROUTE.USERS),
-  async ({ request }) => {
-    // NOTE this populates the user db with 100 users
-    range(10).map(() => db.user.create());
+type ListMockPathParams = any;
+type ListMockRequestBodyType = any;
+type ListMockResponseBodyType = any;
 
-    const url = new URL(request.url);
+export const usersListMockHandler = http.get<
+  ListMockPathParams,
+  ListMockRequestBodyType,
+  ListMockResponseBodyType
+>(mockAPIBaseJoinPath(API_ROUTE.USERS), async ({ request }) => {
+  // NOTE this populates the user db with 100 users
+  range(100).map(() => db.user.create());
 
-    const page = Number(url.searchParams.get("page") || 1);
-    const perPage = Number(url.searchParams.get("perPage") || 10);
-    // const sortBy = url.searchParams.get("sortBy");
+  const url = new URL(request.url);
 
-    const list = db.user.findMany({
-      take: perPage,
-      skip: computeOffset(perPage, page),
-    });
+  const page = Number(url.searchParams.get("page") || 1);
+  const perPage = Number(url.searchParams.get("perPage") || 10);
+  // const sortBy = url.searchParams.get("sortBy");
 
-    const total_records = db.user.count();
+  const list = db.user.findMany({
+    take: perPage,
+    skip: computeOffset(perPage, page),
+  });
 
-    return HttpResponse.json<PaginationResponse>(
-      {
-        records: list,
-        pagination: {
-          page,
-          total_records,
-          total_pages: computeTotalPages(total_records, perPage),
-        },
+  const total_records = db.user.count();
+
+  return HttpResponse.json<PaginationResponse>(
+    {
+      records: list,
+      pagination: {
+        page,
+        total_records,
+        total_pages: computeTotalPages(total_records, perPage),
       },
-      // { status: 500 },
-    );
-  },
-);
+    },
+    // { status: 500 },
+  );
+});
