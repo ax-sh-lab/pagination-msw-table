1b230d461ed9941c3d99d507e49064330c20442f
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-11-06 16:20:36 +0545

    feat(eslint): add testing-library rules and sort imports
    - Added testing-library rules for await-async-queries, no-await-sync-queries,
      no-debugging-utils, and no-dom-import
    - Sorted imports using simple-import-sort/imports rule
    
    Issue: #develop

--- src/ui/state-wrapper.tsx
+++ src/ui/state-wrapper.tsx
@@ -1,12 +1,12 @@
-import { ComponentProps } from "react";
+import { type ComponentProps } from 'react';
 
-export function LoadingWrapper(props: ComponentProps<"div">) {
-  return <div data-testid={"LoadingWrapper"} {...props} />;
+export function LoadingWrapper(props: ComponentProps<'div'>) {
+  return <div data-testid={'LoadingWrapper'} {...props} />;
 }
-export function ErrorWrapper(props: ComponentProps<"div">) {
-  return <div data-testid={"ErrorWrapper"} {...props} />;
+export function ErrorWrapper(props: ComponentProps<'div'>) {
+  return <div data-testid={'ErrorWrapper'} {...props} />;
 }
 
-export function SuccessWrapper(props: ComponentProps<"div">) {
-  return <div data-testid={"SuccessWrapper"} {...props} />;
+export function SuccessWrapper(props: ComponentProps<'div'>) {
+  return <div data-testid={'SuccessWrapper'} {...props} />;
 }
