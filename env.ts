7226393a509cdfa9068b82d7ebe4f08c9ca963b3
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-11-06 15:36:42 +0545

    feat(package.json): Add depcheck script (develop)
    chore(env.ts): Fix import statement (develop)
    
    chore(.prettierrc): Update Prettier configuration (develop)
    
    chore(.depcheckrc): Add depcheck configuration file (develop)

--- env.ts
+++ env.ts
@@ -1,8 +1,8 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z, ZodError } from "zod";
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z, type ZodError } from 'zod';
 
 export const env = createEnv({
-  isServer: typeof window === "undefined",
+  isServer: typeof window === 'undefined',
   server: {
     // DATABASE_URL: z.string().url(),
     // OPEN_AI_API_KEY: z.string().min(1),
@@ -17,16 +17,11 @@ export const env = createEnv({
     NEXT_PUBLIC_API_MOCKING: process.env.NEXT_PUBLIC_API_MOCKING,
   },
   onValidationError: (error: ZodError) => {
-    console.error(
-      "❌ Invalid environment variables:",
-      error.flatten().fieldErrors,
-    );
-    throw new Error("Invalid environment variables");
+    console.error('❌ Invalid environment variables:', error.flatten().fieldErrors);
+    throw new Error('Invalid environment variables');
   },
   // Called when server variables are accessed on the client.
   onInvalidAccess: (variable: string) => {
-    throw new Error(
-      "❌ Attempted to access a server-side environment variable on the client",
-    );
+    throw new Error('❌ Attempted to access a server-side environment variable on the client');
   },
 });
